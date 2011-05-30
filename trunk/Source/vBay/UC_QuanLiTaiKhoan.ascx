071e2b0106229f85688348d897ccbcf40eca911f
anhkhoahome <anhkhoahome@df807081-fcf6-5990-02e3-4d185aaff80b>
2011-05-30 03:29:12 +0000

    Thêm các User Coltrol sau: 1. Cập nhật thông tin tài khoản 2. Đăng thông tin sản phẩm mới 3. Quản lí tài khoản (lock hoặc Unlock 1 tài khoản) 4. Theo dõi lịch sử giao dịch của tài khoản

---
+++ trunk/Source/vBay/UC_QuanLiTaiKhoan.ascx
@@ -0,0 +1,77 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UC_QuanLiTaiKhoan.ascx.cs" Inherits="vBay.UC_QuanLiTaiKhoan" %>
+<link href="Styles/UC_QuanLiTaiKhoan.css" rel="stylesheet" type="text/css" />
+<div id="QuanLiTaiKhoan_MainContent">
+    <%-- Thông điệp tạm --%>    <%--<asp:Label ID="Label1" runat="server" Text="Trần Anh Khoa" ForeColor="White" BackColor="Black"></asp:Label>--%><%-- Nội dung chính --%>
+    <br />
+    <div id="QuanLiTaiKhoan_MainLockDiv">
+        <center>
+            <asp:Label ID="Label2" runat="server" Text="TIẾN HÀNH KHÓA TÀI KHOẢN" 
+                Font-Bold="True" BackColor="Black" ForeColor="White" Font-Size="20px"></asp:Label>
+            <br />
+        </center>
+        <asp:Label ID="Label3" runat="server" Text="Tên tài khoản cần khóa:" 
+            Width="40%" BackColor="Black" ForeColor="White"></asp:Label>
+        <asp:DropDownList ID="DropDownList_TenTaiKhoanCanKhoa" runat="server" 
+            BackColor="White" DataTextField="TenTaiKhoan" DataValueField="MaTaiKhoan" 
+            ForeColor="Black" Width="57%">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Thời hạn khóa:" Width="40%" 
+            BackColor="Black" ForeColor="White"></asp:Label>
+        <asp:DropDownList ID="DropDownList_ThoiHanKhoa" runat="server" 
+            BackColor="White" ForeColor="Black" Width="50px">
+        </asp:DropDownList>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label5" runat="server" Text="Đơn vị:" BackColor="Black" 
+            ForeColor="White"></asp:Label>
+        <asp:DropDownList ID="DropDownList_DonViThoiGian" runat="server" 
+            BackColor="White" ForeColor="Black" Width="70px">
+            <asp:ListItem>Tuần</asp:ListItem>
+            <asp:ListItem>Tháng</asp:ListItem>
+            <asp:ListItem>Vĩnh viễn</asp:ListItem>
+        </asp:DropDownList>
+        <br /><br />
+        <center>
+            <asp:Button ID="Button_Lock" runat="server" Text="Khóa tài khoản" 
+                onclick="Button_Lock_Click" />
+        </center>
+    </div>
+    <br />
+    <div id="QuanLiTaiKhoan_LockedDiv">
+        <center>
+            <asp:Label ID="Label6" runat="server" 
+                Text="DANH SÁCH CÁC TÀI KHOẢN ĐANG BỊ KHÓA" BackColor="Black" Font-Bold="True" 
+                ForeColor="White" Font-Size="20px"></asp:Label>
+            <br /><br />
+            <asp:GridView ID="GridView_DanhSachTaiKhoanBiKhoa" runat="server" 
+                AllowPaging="True" AutoGenerateColumns="False" BackColor="White" 
+                ForeColor="Black" 
+                onpageindexchanging="GridView_DanhSachTaiKhoanBiKhoa_PageIndexChanging" 
+                Width="100%">
+                <Columns>
+                    <asp:BoundField DataField="TenTaiKhoan" HeaderText="Tên tài khoản" 
+                        InsertVisible="False" ReadOnly="True">
+                    <ControlStyle Width="65%" />
+                    </asp:BoundField>
+                    <asp:BoundField DataField="ThoiDiemUnlock" HeaderText="Thời hạn Unlock" 
+                        InsertVisible="False" ReadOnly="True">
+                    <ControlStyle Width="35%" />
+                    </asp:BoundField>
+                </Columns>
+            </asp:GridView>            
+        </center>
+        <br /><br />
+        <asp:Label ID="Label1" runat="server" Text="Chọn tài khoản cần Unlock:" 
+            BackColor="Black" Font-Bold="True" ForeColor="White" Width="40%"></asp:Label>
+        <asp:DropDownList ID="DropDownList_DanhSachTaiKhoanLocked" runat="server" 
+            BackColor="White" DataTextField="TenTaiKhoan" DataValueField="MaTaiKhoan" 
+            ForeColor="Black" Width="57%">
+        </asp:DropDownList>
+        <br /><br />
+        <center>
+            <asp:Button ID="Button_Unlock" runat="server" Text="Unlock" 
+                onclick="Button_Unlock_Click" />
+        </center>
+    </div>
+</div>
+
---
+++ trunk/Source/vBay/UC_QuanLiTaiKhoan.ascx.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+
+namespace vBay
+{
+    public partial class UC_QuanLiTaiKhoan : System.Web.UI.UserControl
+    {
+        DataEntityDataContext dataContext;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            //Load CSDL từ server
+            dataContext = new DataEntityDataContext();
+
+            //Đọc danh sách các tài khoản chưa bị xóa và chưa bị khóa
+            var acc_ChuaBiXoa_ChuaBiKhoa = from tk in dataContext.TaiKhoans
+                                           where tk.BiXoa == false && tk.BiKhoa == false
+                                           select new { tk.MaTaiKhoan, tk.TenTaiKhoan };
+
+            //Đọc danh sách các tài khoản chưa bị xóa và đang bị khóa
+            var acc_ChuaBiXoa_Locked = from tk in dataContext.TaiKhoans
+                                       where tk.BiXoa == false && tk.BiKhoa == true
+                                       select new { tk.MaTaiKhoan, tk.TenTaiKhoan, tk.ThoiDiemUnlock };
+
+            //Cập nhật danh sách tài khoản chưa bị khóa cho DropDownList_TenTaiKhoanCanKhoa
+            //DropDownList_TenTaiKhoanCanKhoa.DataSource = acc_ChuaBiXoa_ChuaBiKhoa;
+            DropDownList_TenTaiKhoanCanKhoa.DataSource = acc_ChuaBiXoa_ChuaBiKhoa;
+            DropDownList_TenTaiKhoanCanKhoa.DataBind();
+
+            //Cập nhật danh sách tài khoản đang bị khóa GridView_DanhSachTaiKhoanBiKhoa
+            GridView_DanhSachTaiKhoanBiKhoa.DataSource = acc_ChuaBiXoa_Locked;
+            GridView_DanhSachTaiKhoanBiKhoa.DataBind();
+            int page = GridView_DanhSachTaiKhoanBiKhoa.PageIndex;
+
+            //Cập nhật danh sách tài khoản đang bị khóa cho DropDownList_DanhSachTaiKhoanLocked
+            DropDownList_DanhSachTaiKhoanLocked.DataSource = acc_ChuaBiXoa_Locked;
+            DropDownList_DanhSachTaiKhoanLocked.DataBind();
+
+            //Cập nhật items cho DropDownList_ThoiHanKhoa
+            for (int i = 1; i <= 12; i++)
+                DropDownList_ThoiHanKhoa.Items.Add(i.ToString());
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView_DanhSachTaiKhoanBiKhoa.DataBind();
+        }
+        protected void GridView_DanhSachTaiKhoanBiKhoa_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView_DanhSachTaiKhoanBiKhoa.PageIndex = e.NewPageIndex;
+
+            //Chỉ update lại Gridview_DanhSachTaiKhoanBiKhoa => Gọi Page_Load
+            Page_Load(sender, e);
+        }
+        protected void Button_Lock_Click(object sender, EventArgs e)
+        {
+            //Lấy mã tài khoản cần khóa, thời gian khóa và đơn vị thời gian
+            int maTaiKhoan = int.Parse(DropDownList_TenTaiKhoanCanKhoa.SelectedItem.Value.ToString());
+            int thoiHanLock = int.Parse(DropDownList_ThoiHanKhoa.SelectedItem.Value.ToString());
+            String donVi = DropDownList_DonViThoiGian.SelectedItem.Text.ToString();
+
+            LockTaiKhoan(maTaiKhoan, thoiHanLock, donVi);
+
+            Page_Init(sender, e);
+        }
+        protected void Button_Unlock_Click(object sender, EventArgs e)
+        {
+            //Lấy thông tin tài khoản được chọn
+            int maTaiKhoan = int.Parse(DropDownList_DanhSachTaiKhoanLocked.SelectedItem.Value.ToString());
+
+            UnlockTaiKhoan(maTaiKhoan);
+
+            Page_Init(sender, e);
+        }
+        public void LockTaiKhoan(int maTaiKhoan, int thoiHanLock, String donVi)
+        {
+            TaiKhoan acc = dataContext.TaiKhoans.Single(p => p.MaTaiKhoan == maTaiKhoan);
+
+            //Nếu đơn vị thời gian là "Vĩnh viễn" => Xóa tài khoản
+            if (donVi == "Vĩnh viễn")
+                acc.BiXoa = true;
+            else
+            {
+                //Nếu đơn vị thời gian khác "Vĩnh viễn" => Tiến hành xóa tài khoản
+                if (donVi != "Vĩnh viễn")
+                {
+                    acc.BiKhoa = true;
+
+                    switch (donVi)
+                    {
+                        case "Tuần":
+                            {
+                                DateTime time = DateTime.Now.AddDays(thoiHanLock * 7);
+                                acc.ThoiDiemUnlock = time;
+
+                                break;
+                            }
+                        case "Tháng":
+                            {
+                                DateTime time = DateTime.Now.AddMonths(thoiHanLock);
+                                acc.ThoiDiemUnlock = time;
+
+                                break;
+                            }
+                    }
+                }
+            }
+
+            dataContext.SubmitChanges();
+        }
+        public void UnlockTaiKhoan(int maTaiKhoan)
+        {
+            TaiKhoan acc = dataContext.TaiKhoans.Single(p => p.MaTaiKhoan == maTaiKhoan);
+
+            acc.BiKhoa = false;
+
+            dataContext.SubmitChanges();
+        }
+    }
+}
