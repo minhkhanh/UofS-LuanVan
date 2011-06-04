8bb4246fe57e2b661b2865bb2652c40dbfc0381f
nguyendinhluyen0808@gmail.com <nguyendinhluyen0808@gmail.com@df807081-fcf6-5990-02e3-4d185aaff80b>
2011-06-04 08:49:16 +0000

    Chỉnh sữa

--- Rap/vBay/WUC/TrangThongKeBaoCaoManager.ascx.designer.cs
+++ Rap/vBay/WUC/TrangThongKeBaoCaoManager.ascx.designer.cs
@@ -7,20 +7,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace vBay
-{
+namespace vBay {
     
     
     public partial class TrangThongKeBaoCaoManager {
-        
-        /// <summary>
-        /// PanelThongke control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel PanelThongke;
         
         /// <summary>
         /// DropDownDanhSachThang control.
@@ -84,6 +74,15 @@ namespace vBay
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button BtCapNhat;
+        
+        /// <summary>
+        /// PanelThongke control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel PanelThongke;
         
         /// <summary>
         /// LabelDoanhThu control.
