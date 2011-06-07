6eb5c5526c66feed79a63964cabfdd53a07fede3
onmyway133@gmail.com <onmyway133@gmail.com@df807081-fcf6-5990-02e3-4d185aaff80b>
2011-06-07 15:55:20 +0000

    DK sidebar menu

--- trunk/Rap/vBay/Site.Master.designer.cs
+++ trunk/Rap/vBay/Site.Master.designer.cs
@@ -49,6 +49,15 @@ namespace vBay {
         protected global::vBay.WUCDangNhapNhanh WUCDangNhapNhanh1;
         
         /// <summary>
+        /// menuDSLoaiSanPham control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu menuDSLoaiSanPham;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
