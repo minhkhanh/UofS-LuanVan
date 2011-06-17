86f139f6216ae016a288d34e57332e647aadd78e
anhkhoahome <anhkhoahome@df807081-fcf6-5990-02e3-4d185aaff80b>
2011-06-17 12:09:55 +0000

    Cập nhật phân quyền cho trang Muốn sửa đổi trang chuyển đến nếu vi phạm quyền, sửa đường dẫn url trong biến pageTurnBackIfNotSignedIn

--- trunk/Rap/vBay/ThayDoiGiaoDienWebSite.aspx.cs
+++ trunk/Rap/vBay/ThayDoiGiaoDienWebSite.aspx.cs
@@ -4,11 +4,57 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 namespace vBay
 {
     public partial class ThayDoiGiaoDienWebSite : System.Web.UI.Page
     {
+        private static String pageTurnBackIfNotSignedIn = "~/Default.aspx";
+
+        public void Page_PreInit()
+        {
+            //Hướng xử lí trong hàm Page_PreInit
+            //  1. Lấy UserName của người dùng và lưu vào String userName
+            //  2. Kiểm tra: Nếu người dùng chưa đăng nhập (UserName == null hoặc là chuỗi rỗng)
+            //      2.1. Respone sang trang pageTurnBackIfNotSignedIn
+            //  3. Lấy danh sách các Role người dùng thuộc về và lưu vào mảng String[] danhSachRoles
+            //  4. Kiểm tra: Nếu người dùng thuộc User Administrator hoặc Manager => Response sang trang pageTurnBackIfNotSignedIn
+
+
+            //Tiến hành
+            //  1. Lấy UserName của người dùng và lưu vào String userName
+            String userName = Page.User.Identity.Name;
+
+            //  2. Kiểm tra: Nếu người dùng chưa đăng nhập (UserName == null hoặc là chuỗi rỗng)
+            if (userName == "" || userName == null)
+            {
+                //      2.1. Respone sang trang pageTurnBackIfNotSignedIn
+                Response.Redirect(pageTurnBackIfNotSignedIn);
+            }
+            else
+            {
+                //  3. Lấy danh sách các Role người dùng thuộc về và lưu vào mảng String[] danhSachRoles
+                String[] danhSachRoles = Roles.GetRolesForUser(userName);
+
+                //  4. Kiểm tra: Nếu người dùng thuộc User Administrator hoặc Manager => Response sang trang pageTurnBackIfNotSignedIn
+                bool isManager = false;
+                bool isAdmin = false;
+                foreach (String i in danhSachRoles)
+                {
+                    if (i == "Administrator")
+                        isAdmin = true;
+                    else if (i == "Manager")
+                        isManager = true;
+                }
+
+                if (isAdmin == false && isManager == false)
+                    Response.Redirect(pageTurnBackIfNotSignedIn);
+
+            }
+
+        }
+
         protected void Page_Init(object sender, EventArgs e)
         {
             
